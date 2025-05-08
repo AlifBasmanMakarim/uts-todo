6d2c167bfc8dfdd51edbd103035449a8e71227ee
AlifBasmanMakarim <alifbasmanmakarim@student.uir.ac.id>
2025-05-08 12:06:56 +0700

    menambahkan struktur dasar komponen dan gaya awal

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="app">
+    <h1 class="title">Kegabutan</h1>
+  </div>
+</template>
+
+<style scoped>
+.app {
+  max-width: 500px;
+  margin: 2rem auto;
+  padding: 1.5rem;
+  border-radius: 12px;
+  background: rgb(0, 21, 5);
+  box-shadow: 0 4px 10px rgba(2, 166, 172, 0.904);
+  font-family: 'Segoe UI', sans-serif;
+}
+
+.title {
+  text-align: center;
+  color: #d4df07;
+  margin-bottom: 1rem;
+}
+</style>
