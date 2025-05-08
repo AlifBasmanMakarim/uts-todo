a84d39fb43e77daa16ab92f235b98baad0d94f5f
AlifBasmanMakarim <alifbasmanmakarim@student.uir.ac.id>
2025-05-08 12:15:42 +0700

    menambahkan struktur awall web

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,26 @@
 <template>
   <div class="app">
     <h1 class="title">Kegabutan</h1>
-    <div class="input-group">
-      <input v-model="newTask" @keyup.enter="addTask" placeholder="Tambahkan kegiatan baru..." />
-      <button @click="addTask">Tambah</button>
-    </div>
-
-    <ul class="task-list">
-      <li v-for="(task, index) in tasks" :key="index" class="task-item">
-        {{ task.text }}
-      </li>
-    </ul>
   </div>
 </template>
 
 <script setup>
-import { ref } from 'vue'
-
-const newTask = ref('')
-const tasks = ref([])
-
-function addTask() {
-  if (newTask.value.trim()) {
-    tasks.value.push({ text: newTask.value, completed: false })
-    newTask.value = ''
-  }
-}
 </script>
 
 <style scoped>
-/* Tambahkan */
-.task-list {
-  list-style: none;
-  padding: 0;
+.app {
+  max-width: 500px;
+  margin: 2rem auto;
+  padding: 1.5rem;
+  border-radius: 12px;
+  background: rgb(0, 21, 5);
+  box-shadow: 0 4px 10px rgba(2, 166, 172, 0.904);
+  font-family: 'Segoe UI', sans-serif;
 }
 
-.task-item {
-  padding: 0.5rem 0;
-  border-bottom: 1px solid #3607e1;
-  color: white;
+.title {
+  text-align: center;
+  color: #d4df07;
+  margin-bottom: 1rem;
 }
 </style>
